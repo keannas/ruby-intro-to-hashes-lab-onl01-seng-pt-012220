734723b7957069ef69c817047d345a29d08e7492
Keanna Salter <keannasalter91@gmail.com>
2020-02-03 03:48:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,17 +21,16 @@ def monopoly_with_second_tier
 		:rent_in_dollars => {}
 		}
 	}
-# # end
-
-# # def monopoly_with_third_tier
+end
+ def monopoly_with_third_tier
 
 		:railroads =>{
 		:pieces => 4,
 		:names=> {},
 		:rent_in_dollars => {}
-# # end
+end
 
-# # def monopoly_with_fourth_tier
-# # 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+def monopoly_with_fourth_tier
+	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
-# # end
+end
