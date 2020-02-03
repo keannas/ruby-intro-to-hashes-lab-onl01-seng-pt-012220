7ba25ccb4135736f65cd6a456a4287de4cacd813
Keanna Salter <keannasalter91@gmail.com>
2020-02-03 04:15:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,7 @@ def monopoly_with_fourth_tier
 		 :pieces => 4,
 		 :names=> {
 		   :reading_railroad => {},
-		   :pennsylvania_railroad => {}
+		   :pennsylvania_railroad => {},
 		   :b_and_o_railroad => {},
 		   :shortline => {}
 		 },
@@ -58,6 +58,4 @@ def monopoly_with_fourth_tier
 		}
 	}
 end
-		 }
-
-end
+		 
