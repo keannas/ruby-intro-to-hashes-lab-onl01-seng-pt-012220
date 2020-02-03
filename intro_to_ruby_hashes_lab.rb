3802ebe76bfcc9fa415941bc21652c566e091408
Keanna Salter <keannasalter91@gmail.com>
2020-02-03 04:20:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,9 +51,11 @@ def monopoly_with_fourth_tier
 		     'mortgage_value' => "$100"
 		   },
 		   :pennsylvania_railroad => {
+		      'mortgage_value' => "$100"
+		   },
+		   :b_and_o_railroad => {
 		     
 		   },
-		   :b_and_o_railroad => {},
 		   :shortline => {}
 		 },
 		 :rent_in_dollars => {
